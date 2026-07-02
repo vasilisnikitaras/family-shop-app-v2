d29aff5ed56dc55b9304d3927ac93acfebbb774b
vasilisnikitaras <vasilis.nikitaras@gmail.com>
2026-07-02 19:55:40 -0400

    Rename old next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 🔥 Απενεργοποιεί Turbopack πλήρως
   experimental: {
     serverActions: {
       bodySizeLimit: "2mb",
     },
   },
-  webpack: (config) => {
-    return config; // 🔥 Ενεργοποιεί Webpack mode
+
+  webpack: (config: any) => {
+    return config;
   },
 };
 
