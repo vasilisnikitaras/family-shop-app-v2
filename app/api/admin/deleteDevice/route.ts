2c4a842a26c229f4d5fc482a7695c910b6d6b686
vasilisnikitaras <vasilis.nikitaras@gmail.com>
2026-08-02 14:51:13 -0400

    Fix deleteDevice route to use Neon

--- app/api/admin/deleteDevice/route.ts
+++ app/api/admin/deleteDevice/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
-import { sql } from "@vercel/postgres";
+import { neon } from "@neondatabase/serverless";
+
+const sql = neon(process.env.DATABASE_URL!);
 
 export async function POST(req: Request) {
   try {
