6acf6002c9648efae1fba504ce8886aaec386d83
vasilisnikitaras <vasilis.nikitaras@gmail.com>
2026-08-02 14:46:44 -0400

    Fix missing sql import

--- app/api/admin/deleteDevice/route.ts
+++ app/api/admin/deleteDevice/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { neon } from "@neondatabase/serverless";
+import { sql } from "@vercel/postgres";
 
 export async function POST(req: Request) {
   try {
